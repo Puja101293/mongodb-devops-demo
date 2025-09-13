5408ff910916ba82e6774683e39dc6bc321a7a20
Puja101293 <Pchandeshri@gmail.com>
2025-09-13 09:32:42 +0000

    chore(userdata): update MongoDB container setup with custom credentials and logging

--- userdata.sh
+++ userdata.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
-# Install Docker
+# Update system and install Docker
 yum update -y
 amazon-linux-extras enable docker
 yum install -y docker
 systemctl start docker
 systemctl enable docker
 
-# Run MongoDB container
-docker run -d --name mongodb -p 27017:27017 -e MONGO_INITDB_ROOT_USERNAME=admin -e MONGO_INITDB_ROOT_PASSWORD=password mongo:6.0
+# Add ec2-user to docker group (so you don’t always need sudo)
+usermod -aG docker ec2-user
+
+# Run MongoDB container with custom credentials
+docker run -d \
+  --name mongodb \
+  -p 27017:27017 \
+  -e MONGO_INITDB_ROOT_USERNAME=puja \
+  -e MONGO_INITDB_ROOT_PASSWORD=MyStrongPass123 \
+  mongo:6.0
+
+# Log setup result
+echo "MongoDB container deployed successfully on $(date)" >> /var/log/mongo-setup.log
